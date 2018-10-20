a95f3231a941281d7c436ed464e8f87bcf77d63e
Claudia <Claudia2.Brown@live.uwe.ac.uk>
2018-10-20 18:29:42 +0100

    Print "We are going to build a cuboid"

--- Worksheet 1.cpp
+++ Worksheet 1.cpp
@@ -2,12 +2,13 @@
 // Created by Claudia Brown on 20/10/2018.
 //
 
-#include <fstring>
+#include <iostream>
 
 int main() {
 
     std::cout << "We are going to build a cuboid -" ;
-
+    std::cout << "Please enter Width";
+    std::cin >> Width;
 
     return(0);
 };
