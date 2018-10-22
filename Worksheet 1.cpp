53704ab36bf8a351b388563ffa7efa83a60b662b
Claudia <Claudia2.Brown@live.uwe.ac.uk>
2018-10-22 16:04:26 +0100

    Added to W, H and L

--- Worksheet 1.cpp
+++ Worksheet 1.cpp
@@ -8,10 +8,26 @@
 
 int main() {
 
+    float Width = 0.0;
+    float Height = 0.0;
+    float Length = 0.0;
+
+
     std::cout << "We are going to build a cuboid -" ;
     std::cout << "Please enter Width";
     std::cin >> Width;
+
     float Width "%f";
+
+    std::cout << "Please enter Height";
+    std::cin >> Height;
+
+    float Height "%f";
+
+    std::cout << "Please enter Length";
+    std::cin >> Length;
+
+    float Length "%f";
 
     return(0);
 };
